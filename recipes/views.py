262c2f19f5c1d7cfb09fd32253046e9c20558661
Jonathas Emanuel <jonathas92244@gmail.com>
2023-12-19 06:25:20 -0300

    criada nova view para uma nova URL

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,8 @@ def home(request):
     return render(request, 'pages/home.html', context={
         'name' : 'Jonathas Emanuel'
     })
+
+def recipe(request, id):
+    return render(request, 'pages/home.html', context={
+        'name' : 'Jonathas Emanuel'
+    })
