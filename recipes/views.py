325b68191002e4d44872933d694d76afbb2c85a9
Jonathas Emanuel <jonathas92244@gmail.com>
2023-12-24 16:08:06 -0300

    injetado dados na list da view home

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-
+from utils.recipe.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name' : 'Jonathas Emanuel'
+        'recipes' : [make_recipe() for _ in range(10)]
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name' : 'Jonathas Emanuel'
+        'recipe' : make_recipe(),
     })
