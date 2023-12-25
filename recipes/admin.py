bfe8314318d38d9e12ee3e21e8dcae6599d634af
Jonathas Emanuel <jonathas92244@gmail.com>
2023-12-25 08:00:17 -0300

    registrado category in admin django

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category
+class CategoryAdmin(admin.ModelAdmin):
+    ...
 
-# Register your models here.
+admin.site.register(Category, CategoryAdmin)
