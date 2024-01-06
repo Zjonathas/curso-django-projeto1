9b9b9721eab91d7c7c25e11aa984bd9f345fdc40
Jonathas Emanuel <jonathas92244@gmail.com>
2024-01-06 10:56:36 -0300

    add 404 para category

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from utils.recipe.factory import make_recipe
 from recipes.models import Recipe
+from django.http import Http404
 
 def home(request):
     recipes = Recipe.objects.filter(is_publish = True).order_by('-id')
@@ -14,12 +15,17 @@ def category(request, category_id):
         category__id=category_id,
         is_publish=True
     ).order_by('-id')
+
+    if not recipes:
+        raise Http404('Category not found')
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.firts().category.name} - Category',
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe' : make_recipe(),
-        'is_detail_page' : True,
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
