fe4fe800f3b4c635942250b8bb2b6df7a6ab53f1
Jonathas Emanuel <jonathas92244@gmail.com>
2024-01-06 07:25:12 -0300

    filter para is_publish

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from utils.recipe.factory import make_recipe
 from recipes.models import Recipe
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_publish = True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -11,9 +11,10 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        category__id=category_id,
+        is_publish=True
     ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
