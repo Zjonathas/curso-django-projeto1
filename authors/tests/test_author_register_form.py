2b2725adff4f13fe6f4eb9e2eb74898a754192b4
Jonathas Emanuel <jonathas92244@gmail.com>
2024-04-11 13:04:51 -0300

    testing field not empty username with test integration

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -1,9 +1,12 @@
 from unittest import TestCase
+from django.test import TestCase as DjangoTestCase
 from authors.forms import RegisterForm
 from parameterized import parameterized
+from django.urls import reverse
 
 
 class AuthorRegisterFormUnitTest(TestCase):
+    # 
     @parameterized.expand([
         ('username', 'Your username'),
         ('email', 'Your e-mail'),
@@ -43,3 +46,25 @@ class AuthorRegisterFormUnitTest(TestCase):
         form = RegisterForm()
         current = form[field].field.label
         self.assertEqual(current, needed)
+
+
+class AuthorRegisterIntegrationTest(DjangoTestCase):
+    def setUp(self, *args, **kwargs):
+        self.form_data = {
+            'username': 'user',
+            'first_name': 'first',
+            'last_name': 'last',
+            'email': 'email@email.com',
+            'password': 'Str0ngP@ssword1',
+            'password2': 'Str0ngP@ssword1',
+        }
+        return super().setUp(*args, **kwargs)
+
+    @parameterized.expand([
+        ('username', 'This field must not be empty')
+    ])
+    def test_field_cannot_be_empty(self, field, msg):
+        self.form_data[field] = ''
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(msg, response.content.decode('utf-8'))
