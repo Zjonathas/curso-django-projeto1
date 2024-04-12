ed8b081ceb799fb7b529e6854c1aeb872bf0745e
Jonathas Emanuel <jonathas92244@gmail.com>
2024-04-12 15:45:01 -0300

    add placegholder login form

--- authors/forms/login_form.py
+++ authors/forms/login_form.py
@@ -1,7 +1,13 @@
 from django import forms
+from utils.django_forms import add_attr, add_placeholder, strong_password
 
 
 class LoginForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['password'], 'Type your password')
+
     username = forms.CharField()
     password = forms.CharField(
         widget=forms.PasswordInput()
