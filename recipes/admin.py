58a124ff33c9cead440084f5d030f0532ca6f113
Jonathas Emanuel <jonathas92244@gmail.com>
2024-04-18 08:38:09 -0300

    adjustments in the administrative area

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from .models import Category, Recipe
 
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'title', 'created_at', 'is_publish', 'author'
+    list_display_links = 'title', 'created_at'
+    search_fields = 'id', 'title', 'description', 'slug', 'preparation_steps'
+    list_filter = 'category', 'author', 'is_publish', \
+        'preparation_steps_html'
+    list_per_page = 10
+    list_editable = 'is_publish',
+    ordering = '-id',
+
+
 class CategoryAdmin(admin.ModelAdmin):
     ...
+
 
 admin.site.register(Category, CategoryAdmin)
