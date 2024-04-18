f0b9704b89bddf1d9740e9c6ae4c931c7fef9c55
Jonathas Emanuel <jonathas92244@gmail.com>
2024-04-18 11:05:16 -0300

    Salvando as edições da receita na base de dados e request.FILES para cov

--- authors/views.py
+++ authors/views.py
@@ -116,8 +116,21 @@ def dashboard_recipe_edit_view(request, id):
 
     form = AuthorRecipeForm(
         data=request.POST or None,
+        files=request.FILES or None,
         instance=recipe
     )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_publish = False
+
+        recipe.save()
+
+        messages.success(request, 'Sua receita foi salva com sucesso.')
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
 
     return render(request, 'authors/pages/dashboard_recipe.html', {
             'form': form,
