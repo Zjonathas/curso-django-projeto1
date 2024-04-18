d4232d48523662dcd4e94c38d5b77bc0a2b7f137
Jonathas Emanuel <jonathas92244@gmail.com>
2024-04-18 08:21:04 -0300

    create page dashboard authors

--- authors/views.py
+++ authors/views.py
@@ -49,7 +49,6 @@ def login_create(request):
         raise Http404()
 
     form = LoginForm(request.POST)
-    login_url = reverse('authors:login')
 
     if form.is_valid():
         authenticated_user = authenticate(
@@ -64,7 +63,7 @@ def login_create(request):
             messages.error(request, 'Invalid credentials')
     else:
         messages.error(request, 'Invalid username or password')
-    return redirect(login_url)
+    return redirect('authors:dashboard')
 
 
 @login_required(login_url='authors:login', redirect_field_name='next')
@@ -80,3 +79,8 @@ def logout_view(request):
     messages.success(request, 'Logged out successfully')
     logout(request)
     return redirect(reverse('authors:login'))
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboar_view(request):
+    return render(request, 'authors/pages/dashboard.html')
