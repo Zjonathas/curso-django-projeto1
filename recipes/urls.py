2aa11bde8e6411ef9eaba6bb57121d19ad5abed4
Jonathas Emanuel <jonathas92244@gmail.com>
2024-04-22 18:23:36 -0300

    Adicionando JSONResponse para a API que usa DetailView

--- recipes/urls.py
+++ recipes/urls.py
@@ -24,4 +24,8 @@ urlpatterns = [
         'recipes/api/v1',
         views.RecipeListViewHomeApi.as_view(),
         name="recipe_api_v1"),
+    path(
+        'recipes/api/v1/<int:pk>/',
+        views.RecipeDetailAPI.as_view(),
+        name="recipe_api_v1_detail"),
 ]
--- recipes/views/detail_view_recipe.py
+++ recipes/views/detail_view_recipe.py
@@ -1,7 +1,10 @@
-from django.http.response import Http404
+from typing import Any
+from django.http.response import Http404, HttpResponse as HttpResponse
 from django.db.models.query import QuerySet
 from django.views.generic import DetailView
+from django.http import JsonResponse
 from recipes.models import Recipe
+from django.forms.models import model_to_dict
 
 
 class RecipeDetail(DetailView):
@@ -22,3 +25,26 @@ class RecipeDetail(DetailView):
              'is_detail_page': True, }
              )
         return context
+
+
+class RecipeDetailAPI(RecipeDetail):
+    def render_to_response(self, context, **response_kwargs):
+        recipe = self.get_context_data()['recipe']
+        recipe_dict = model_to_dict(recipe)
+
+        recipe_dict['created_at'] = str(recipe.created_at)
+        recipe_dict['updated_at'] = str(recipe.updated_at)
+
+        if recipe_dict.get('cover'):
+            recipe_dict['cover'] = self.request.build_absolute_uri() + \
+                recipe_dict['cover'].url[1:]
+        else:
+            recipe_dict['cover'] = ''
+
+        del recipe_dict['is_published']
+        del recipe_dict['preparation_steps_is_html']
+
+        return JsonResponse(
+            recipe_dict,
+            safe=False,
+        )
