1da3c025e1cc0e182eb9f22804dba81cd84298d2
Jonathas Emanuel <jonathas92244@gmail.com>
2024-05-03 13:12:00 -0300

    instalando django rest framework

--- project/settings/installed_apps.py
+++ project/settings/installed_apps.py
@@ -1,12 +1,16 @@
 # Application definition
 
 INSTALLED_APPS = [
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Django rest framework
+    'rest_framework',
+    # These are our apps
     'recipes',
     'authors',
     'tag',
