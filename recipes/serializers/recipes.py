5502798799567fa92590003f272228eefcd51a42
Jonathas Emanuel <jonathas92244@gmail.com>
2024-05-11 08:53:26 -0300

    Migrando para ModelSerializer ao invés de Serializer no Django Rest Framework

--- recipes/serializers/recipes.py
+++ recipes/serializers/recipes.py
@@ -1,35 +1,35 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from tag.models import Tag
-
-class TagSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=255)
-    slug = serializers.SlugField()
+from recipes.models import Recipe
 
 
-class RecipeSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=65)
-    description = serializers.CharField(max_length=165)
-    public = serializers.BooleanField(source='is_publish')
-    preparation = serializers.SerializerMethodField()
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name', 'slug', ]
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'description',
+                  'is_publish', 'preparation_time',
+                  'preparation_time_unit', 'category',
+                  'author', 'tags', 'public', 'preparation',
+                  'tag_objects', 'tag_links']
+    public = serializers.BooleanField(source='is_publish', read_only=True)
+    preparation = serializers.SerializerMethodField(
+        method_name='get_preparation', read_only=True)
     category = serializers.StringRelatedField()
-    author = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all()
-    )
-    tags = serializers.PrimaryKeyRelatedField(
-        queryset=Tag.objects.all(),
-        many=True
-    )
     tag_objects = TagSerializer(
-        many=True, source='tags'
+        many=True, source='tags', read_only=True
     )
     tag_links = serializers.HyperlinkedRelatedField(
         many=True,
         source='tags',
-        queryset=Tag.objects.all(),
         view_name='recipes:recipe_api_v2_tag',
+        read_only=True
     )
 
     def get_preparation(self, recipe):
