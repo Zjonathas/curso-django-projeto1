06f1c13e5fa509a910a6790685377843ca02f0d0
Jonathas Emanuel <jonathas92244@gmail.com>
2024-05-23 11:35:08 -0300

    Criando uma URL personalizada no router do Django Rest Framework

--- authors/views/api.py
+++ authors/views/api.py
@@ -2,6 +2,8 @@ from rest_framework.viewsets import ReadOnlyModelViewSet
 from ..serializer import AuthorSerializer
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 
 class AuthorViewSet(ReadOnlyModelViewSet):
@@ -12,3 +14,13 @@ class AuthorViewSet(ReadOnlyModelViewSet):
         User = get_user_model()
         qs = User.objects.filter(username=self.request.user.username)
         return qs
+
+    @action(
+            detail=False,
+            methods=['get'])
+    def me(self, request, *args, **kwargs):
+        obj = self.get_queryset().first()
+        serializer = self.get_serializer(
+            instance=obj
+            )
+        return Response(serializer.data)
